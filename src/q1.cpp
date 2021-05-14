9b929c2f5d2cc0f661555f520c45966de4a7212c
dervis.kopuz <derviskopuz@gmail.com>
2021-05-14 20:50:15 +0300

    örnek tamam

--- src/q1.cpp
+++ src/q1.cpp
@@ -56,7 +56,7 @@ double findMax(double array[], int length)
 int main() {
 
   string fname;
-  fname = "strings.txt";
+  fname = "strings1.txt";
 	//get file name
   //cin >> fname;
   ifstream filename(fname);
@@ -69,10 +69,15 @@ int main() {
 		words.push_back(word);
 	}
 
+	//sort words alphabetically
+	sort(words.begin(), words.end());
+
+	/*
 	//print words
 	for (int i=0; i<words.size(); i++) {
 		cout << words.at(i) << endl;
 	}
+	*/
 
 	/*
 	string word1 = words[1];
@@ -84,6 +89,10 @@ int main() {
 	int firstWordCounter = 0;
 	int secondWordCounter = 0;
 	int secondWordRef = 0;
+	int tick;
+	//vector to hold overlap letters
+	vector<string> overlaps;
+
 	//loop through words
 	for (firstWordCounter; firstWordCounter<words.size(); firstWordCounter++) {
 		//skip over comparing the same words
@@ -94,12 +103,13 @@ int main() {
 
 			word1 = words[firstWordCounter];
 			word2 = words[secondWordCounter];
-			cout << "word1: " << word1 << " word2: " << word2 << endl;
-
 
 			//word lengths
 			int length1 = word1.length();
 			int length2 = word2.length();
+
+			char consensus_a[length1 + length2 + 2];
+			char consensus_b[length1 + length2 + 2];
 
 			//init matrix
 			double matrix[length1+1][length2+1];
@@ -146,7 +156,7 @@ int main() {
 				}
 			}
 
-			
+			/*
 			// print the scoring matrix to console
 			for(int i=0;i<=length1;i++)
 			{
@@ -156,100 +166,151 @@ int main() {
 				}
 				cout << endl;
 			}
+			*/
 
 			//find max score in matrix
 			double matrix_max = 0;
 			int i_max = 0, j_max = 0;
+			int maxScoreCount = 0;
+			//vector to hold index of maximum numbers (i, j)
+			vector<int> iAndJMax;
 			for(int i = 1; i <= length1; i++) {
 				for (int j = 1; j <= length2; j++) {
 
 					if(matrix[i][j] > matrix_max) {
+						//reset score count and vector if maximimum increases
+						maxScoreCount = 0;
+						iAndJMax.clear();
+
 						matrix_max = matrix[i][j];
 						i_max = i;
 						j_max = j;
+						
+						iAndJMax.push_back(i_max);
+						iAndJMax.push_back(j_max);
+
+						//record multiple maximums
+					} else if (matrix[i][j] == matrix_max) {
+						i_max = i;
+						j_max = j;
+						
+						iAndJMax.push_back(i_max);
+						iAndJMax.push_back(j_max);
+						maxScoreCount++;
 					}
 
 				}
 			}
 
-			cout << "Max score in the matrix is " << matrix_max << endl;
+			//cout << "Max score in the matrix is " << matrix_max << endl;
 
-			//traceback
-			int current_i = i_max;
-			int current_j = j_max;
-			int next_i = I_i[current_i][current_j];
-			int next_j = I_j[current_i][current_j];
-			int tick = 0;
+			for (int k = 0; k < iAndJMax.size(); k += 2) {
+				//assign the pair of max i and j
+				i_max = iAndJMax[k];
+				j_max = iAndJMax[k+1];
 
-			char consensus_a[length1 + length2 + 2];
-			char consensus_b[length1 + length2 + 2];
+				//traceback
+				int current_i = i_max;
+				int current_j = j_max;
+				int next_i = I_i[current_i][current_j];
+				int next_j = I_j[current_i][current_j];
+				tick = 0;
 
-			while(((current_i != next_i) || (current_j != next_j)) && (next_i >= 0) && (next_i >= 0)) {
+				while(((current_i != next_i) || (current_j != next_j)) && (next_i >= 0) && (next_i >= 0)) {
 
-				if(next_i == current_i) {
-					//deletion in A
-					consensus_a[tick] = '-';
-				} else {
-					//match/mismatch in A
-					consensus_a[tick] = word1[current_i-1];
+					//cout << word1[current_i-1] << endl;
+
+					if(next_i == current_i) {
+						//deletion in A
+						consensus_a[tick] = '-';
+					} else {
+						//match/mismatch in A
+						consensus_a[tick] = word1[current_i-1];
+					}
+
+					if(next_j == current_j) {
+						//deletion in B
+						consensus_b[tick] = '-';
+					} else {
+						//match/mismatch in B
+						consensus_b[tick] = word2[current_j-1];
+					}
+
+					if (next_i == 0) {
+						next_i = -1;
+					} else if (next_j == 0) {
+						next_j = -1;
+					} else {
+						current_i = next_i;
+						current_j = next_j;
+						next_i = I_i[current_i][current_j];
+						next_j = I_j[current_i][current_j];
+					}
+
+					//push overlaps to the vector
+
+					tick++;
 				}
+				overlaps.push_back(consensus_a);
+				//overlaps.push_back(*consensus_b);
 
-				if(next_j == current_j) {
-					//deletion in B
-					consensus_b[tick] = '-';
-				} else {
-					//match/mismatch in B
-					consensus_b[tick] = word2[current_j-1];
+				//print consensus sequence
+				//cout << "consesus a :" << consensus_a <<endl;
+				//cout << "consesus b :" << consensus_b <<endl;
+				//print output
+				/*
+					cout << word1 << " - " << word2 << endl;
+					cout << "Score: " << tick << " Sequence(s): " ;
+					cout << "\"" ;
+				for (int i = 0; i < overlaps.size(); i++) {
+					*consensus_a = overlaps[i];
+					for(int i = tick-1; i>=0; i--) {
+						cout<<consensus_a[i]; 
+					} 
 				}
+					cout << "\"" << endl;
+					*/
 
-				if (next_i == 0) {
-					next_i = -1;
-				} else if (next_j == 0) {
-					next_j = -1;
-				} else {
-					current_i = next_i;
-					current_j = next_j;
-					next_i = I_i[current_i][current_j];
-					next_j = I_j[current_i][current_j];
+				/*
+				//ptritn conssss
+				cout << consensus_a <<endl;
+				cout << consensus_b <<endl;
+
+				for(int i = tick-1; i>=0; i--) {
+					cout<<consensus_a[i]; 
 				}
-				tick++;
+				cout<<endl;
+				for(int j = tick-1; j>=0; j--) {
+					cout<<consensus_b[j];
+				}
+				cout<<endl;
+				*/
 			}
 
-			//print consensus sequence
-			//cout << "consesus a :" << consensus_a <<endl;
-			//cout << "consesus b :" << consensus_b <<endl;
-			//print output
+			//sort overlaps alphabetically
+			sort(overlaps.begin(), overlaps.end());
+
 			cout << word1 << " - " << word2 << endl;
 			cout << "Score: " << tick << " Sequence(s): " ;
-			cout << "\"" ;
-			for(int i = tick-1; i>=0; i--) {
-				cout<<consensus_a[i]; 
-			} 
-			cout << "\"" << endl;
-
-			/*
-			//ptritn conssss
-			cout << consensus_a <<endl;
-			cout << consensus_b <<endl;
-
-			for(int i = tick-1; i>=0; i--) {
-				cout<<consensus_a[i]; 
+			for (int i = 0; i < overlaps.size(); i++) {
+				string overlapLetters = overlaps[i];
+				cout << "\"" ;
+				for(int k = tick-1; k>=0; k--) {
+					cout<<overlapLetters[k]; 
+				} 
+				cout << "\" ";
 			}
-			cout<<endl;
-			for(int j = tick-1; j>=0; j--) {
-				cout<<consensus_b[j];
-			}
-			cout<<endl;
-			*/
-			cout <<consensus_a<<endl;
-			cout <<consensus_b<<endl;
-			cout <<"the end \n\n"<<endl;
+			cout << endl;
+			overlaps.clear();
+
 		}
 
 		secondWordRef++;
 		secondWordCounter = secondWordRef;
 
 	}
+
+	cout <<"the end";
 	
 	return 0;
 }
